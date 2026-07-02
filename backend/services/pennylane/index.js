ccb4b4ef38633aa68db1072fd260a73a0f32cc9d
ALRICPAON <paonalric@gmail.com>
2026-07-02 20:30:08 +0200

    Synchroniser les factures clients ALTA vers Pennylane
    Synchronise les factures clients finalisees vers Pennylane via la queue existante.
    
    Ajoute la migration, le service dedie, le worker relancable et l'export Pennylane correspondant.

--- backend/services/pennylane/index.js
+++ backend/services/pennylane/index.js
@@ -1,16 +1,19 @@
 const { createPennylaneClient, PennylaneApiError } = require('./client');
 const { getPennylaneConfig } = require('./config');
 const { buildPennylaneCompanyCustomerPayload, processPennylaneClientSyncQueue } = require('./clientSync');
+const { buildPennylaneCustomerInvoicePayload, processPennylaneCustomerInvoiceSyncQueue } = require('./customerInvoiceSync');
 const { buildPennylaneSupplierPayload, processPennylaneSupplierSyncQueue } = require('./supplierSync');
 const { testPennylaneConnection } = require('./testConnection');
 
 module.exports = {
   PennylaneApiError,
   buildPennylaneCompanyCustomerPayload,
+  buildPennylaneCustomerInvoicePayload,
   buildPennylaneSupplierPayload,
   createPennylaneClient,
   getPennylaneConfig,
   processPennylaneClientSyncQueue,
+  processPennylaneCustomerInvoiceSyncQueue,
   processPennylaneSupplierSyncQueue,
   testPennylaneConnection,
 };
