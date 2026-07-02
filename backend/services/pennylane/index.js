264b8297cf799ac5adbdeabe7540d539bc3fd35b
ALRICPAON <paonalric@gmail.com>
2026-07-02 18:03:05 +0200

    Export Pennylane supplier sync helpers

--- backend/services/pennylane/index.js
+++ backend/services/pennylane/index.js
@@ -1,13 +1,16 @@
 const { createPennylaneClient, PennylaneApiError } = require('./client');
 const { getPennylaneConfig } = require('./config');
 const { buildPennylaneCompanyCustomerPayload, processPennylaneClientSyncQueue } = require('./clientSync');
+const { buildPennylaneSupplierPayload, processPennylaneSupplierSyncQueue } = require('./supplierSync');
 const { testPennylaneConnection } = require('./testConnection');
 
 module.exports = {
   PennylaneApiError,
   buildPennylaneCompanyCustomerPayload,
+  buildPennylaneSupplierPayload,
   createPennylaneClient,
   getPennylaneConfig,
   processPennylaneClientSyncQueue,
+  processPennylaneSupplierSyncQueue,
   testPennylaneConnection,
 };
