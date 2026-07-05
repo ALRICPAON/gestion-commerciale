0d8815c85d22d9c0c653eac8623493d32308d5fa
ALRICPAON <paonalric@gmail.com>
2026-07-05 11:59:11 +0200

    Add QMS filters component scaffold

---
+++ frontend/quality/components/QualityFilters.js
@@ -0,0 +1,7 @@
+function createQualityFilters() {
+  const filters = document.createElement('div');
+  filters.className = 'quality-filters';
+  return filters;
+}
+
+window.createQualityFilters = createQualityFilters;
